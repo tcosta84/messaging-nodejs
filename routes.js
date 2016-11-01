bd0a3ec2259e248278f87844eca898e62672229e
Thiago Costa <thiagodacosta@gmail.com>
2016-11-01 16:02:43 -0200

    Add a sample route and test

--- routes.js
+++ routes.js
@@ -1,9 +1,32 @@
 var express = require('express');
+var request = require('request');
+var nock = require('nock');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.send('Express');
+router.put('/sms/send', function(req, res, next) {
+    var options = {
+        'headers': {
+            'Content-type': 'application/json'
+        }
+    };
+    request.put('http://www.tim.com.br/api/v1/sms', options, function(error, response, body){
+        if(error){
+            res.status(500).send({'error': 'Internal Server Error'});
+        } else {
+            if(response.statusCode == 201){
+                res.status(201).json({'description': 'Message sent!'});
+            } else {
+                res.status(205).end();
+            }
+        }
+    });
 });
+
+// nock('http://www.tim.com.br')
+//     .put('/api/v1/sms')
+//     // .replyWithError('Something terrible happened!');
+//     .reply(201, {
+//         'description': 'Message sent!'
+//     });
 
 module.exports = router;
---
+++ test.js
@@ -0,0 +1,38 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var nock = require('nock');
+var app = require('./app');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('/PUT sms', function(){
+
+    beforeEach(function(done){
+        n = nock('http://www.tim.com.br').put('/api/v1/sms');
+        done();
+    });
+
+    it('should send', function(done){
+        n.reply(201);
+
+        chai.request(app).put('/api/v1/sms/send').end(function(err, res){
+            res.should.have.status(201);
+            res.should.be.json;
+            res.body.description.should.equal('Message sent!');
+            done();
+        });
+    });
+
+    it('should not send when external api responds with error', function(done){
+        n.replyWithError;
+
+        chai.request(app).put('/api/v1/sms/send').end(function(err, res){
+            res.should.have.status(500);
+            res.should.be.json;
+            res.body.error.should.equal('Internal Server Error');
+            done();
+        });
+    });
+
+});
