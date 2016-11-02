3e2ba11c513d46bcf36e885a859a3f298e7d1b32
Thiago Costa <thiagodacosta@gmail.com>
2016-11-02 19:49:43 -0200

    Add first version of router and tests

--- routes.js
+++ routes.js
@@ -3,30 +3,40 @@ var request = require('request');
 var nock = require('nock');
 var router = express.Router();
 
-router.put('/sms/send', function(req, res, next) {
+router.put('/sms/send', function(req, res, next){
+
+    // Validation
+    var requiredProps = ['from', 'to', 'body'];
+    for(var prop of requiredProps){
+        if(req.body[prop] == undefined){
+            res.status(400).json({'error': 'Property "' + prop + '" is missing'});
+            return;
+        }
+    }
+
+    if(new Date(req.body.expiration_date) < new Date()){
+        res.status(400).json({'error': 'Expiration date is old'});
+        return;
+    }
+
     var options = {
         'headers': {
             'Content-type': 'application/json'
         }
     };
+
     request.put('http://www.tim.com.br/api/v1/sms', options, function(error, response, body){
         if(error){
             res.status(500).send({'error': 'Internal Server Error'});
+            return;
+        }
+
+        if(response.statusCode == 201){
+            res.status(201).json({'description': 'Message sent!'});
         } else {
-            if(response.statusCode == 201){
-                res.status(201).json({'description': 'Message sent!'});
-            } else {
-                res.status(205).end();
-            }
+            res.status(205).end();
         }
     });
 });
-
-// nock('http://www.tim.com.br')
-//     .put('/api/v1/sms')
-//     // .replyWithError('Something terrible happened!');
-//     .reply(201, {
-//         'description': 'Message sent!'
-//     });
 
 module.exports = router;
--- test.js
+++ test.js
@@ -17,7 +17,8 @@ describe('/PUT sms', function(){
     it('should send', function(done){
         requestMock.reply(201);
 
-        chai.request(app).put('/api/v1/sms/send').end(function(err, res){
+        var data = {'from': '21981527318', 'to': '21980072800', 'body': 'Hello!'};
+        chai.request(app).put('/api/v1/sms/send').send(data).end(function(err, res){
             res.should.have.status(201);
             res.should.be.json;
             res.body.description.should.equal('Message sent!');
@@ -28,10 +29,65 @@ describe('/PUT sms', function(){
     it('should not send when external api responds with error', function(done){
         requestMock.replyWithError;
 
-        chai.request(app).put('/api/v1/sms/send').end(function(err, res){
+        var data = {'from': '21981527318', 'to': '21980072800', 'body': 'Hello!'};
+        chai.request(app).put('/api/v1/sms/send').send(data).end(function(err, res){
             res.should.have.status(500);
             res.should.be.json;
             res.body.error.should.equal('Internal Server Error');
+            done();
+        });
+    });
+
+    it('should not send when data is empty', function(done){
+        var data = '';
+        chai.request(app).put('/api/v1/sms/send').send(data).end(function(err, res){
+            res.should.have.status(400);
+            done();
+        });
+    });
+
+    it('should not send when property "from" is missing', function(done){
+        var data = {'to': '21980072800', 'body': 'Hello!'};
+        chai.request(app).put('/api/v1/sms/send').send(data).end(function(err, res){
+            res.should.have.status(400);
+            res.should.be.json;
+            res.body.error.should.equal('Property "from" is missing');
+            done();
+        });
+    });
+
+    it('should not send when property "to" is missing', function(done){
+        var data = {'from': '21981527318', 'body': 'Hello!'};
+        chai.request(app).put('/api/v1/sms/send').send(data).end(function(err, res){
+            res.should.have.status(400);
+            res.should.be.json;
+            res.body.error.should.equal('Property "to" is missing');
+            done();
+        });
+    });
+
+    it('should not send when property "body" is missing', function(done){
+        var data = {'to': '21980072800', 'from': '21981527318'};
+        chai.request(app).put('/api/v1/sms/send').send(data).end(function(err, res){
+            res.should.have.status(400);
+            res.should.be.json;
+            res.body.error.should.equal('Property "body" is missing');
+            done();
+        });
+    });
+
+    it('should not send when property "expiration_date" is an old date', function(done){
+        var data = {
+            'from': '21981527318',
+            'to': '21980072800',
+            'body': 'Hello!',
+            'expiration_date': '2010-07-14T10:00'
+        };
+
+        chai.request(app).put('/api/v1/sms/send').send(data).end(function(err, res){
+            res.should.have.status(400);
+            res.should.be.json;
+            res.body.error.should.equal('Expiration date is old');
             done();
         });
     });
